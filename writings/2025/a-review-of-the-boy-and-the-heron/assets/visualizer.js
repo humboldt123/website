c987216c91c2012c5abc47e1f16662888c479ba1
Vish <38266782+humboldt123@users.noreply.github.com>
2025-03-12 22:08:09 -0400

    fix: weird desync

--- writings/2025/a-review-of-the-boy-and-the-heron/assets/visualizer.js
+++ writings/2025/a-review-of-the-boy-and-the-heron/assets/visualizer.js
@@ -1,85 +1,96 @@
 // get mm from global scope
 const mm = window.core;
-let last_index = -1;
+let currentlyPlayingIndex = -1;
 
-// we should just mute this 
+
 const player = new mm.SoundFontPlayer(
-	'https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus',
-	mm.Player.tone.Master, null, null, {
-		run: (note) => {
-			for (let i = 0; i < visualizers.length; i++) {
-				visualizers[i].redraw(note, true);
-			}
-		},
-		stop: () => {
-			for (let i = 0; i < visualizers.length; i++) {
-				visualizers[i].clearActiveNotes();
-			}
-		}
-	}
+    'https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus',
+    undefined,
+    null, null, {
+        run: (note) => {
+            if (currentlyPlayingIndex >= 0 && currentlyPlayingIndex < visualizers.length) {
+                visualizers[currentlyPlayingIndex].redraw(note, true);
+            }
+        },
+    }
 );
 
-const jukebox = document.getElementById('jukebox');
-
 const staffs = document.getElementsByClassName("staff");
-const visualizers = new Array(staffs.length); // will be popualted with motifs
-const sequences = new Array(staffs.length); 
+const visualizers = new Array(staffs.length);
+const sequences = new Array(staffs.length);
+const audioElements = new Array(staffs.length);
 
-const music_players = new Array(staffs.length); 
 
 for (let i = 0; i < staffs.length; i++) {
-	let staff = staffs[i];
-	staff.addEventListener('click', () => startOrStop(i));
+    const staff = staffs[i];
+    const motif = staff.getAttribute("motif");
+    
 
-	let motif = staff.getAttribute("motif");
-
-	const audioElement = Object.assign(document.createElement('audio'), { 
-		style: "display: none;", 
-		src: `/writings/2025/a-review-of-the-boy-and-the-heron/assets/motifs/${motif}.mp3`,
-		controls: true,
-		volume: 0.5 
-	});
-
-	audioElement.onloadeddata = function() {
-		music_players[i] = this;
-	};
-
-	staff.parentNode.appendChild(audioElement);
-
-	mm.urlToNoteSequence(`/writings/2025/a-review-of-the-boy-and-the-heron/assets/motifs/${motif}.mid`)
-	  .then(seq => {
-		visualizers[i] = new mm.StaffSVGVisualizer(seq, staff);
-		sequences[i] = seq;
-		const tempo = seq.tempos[0].qpm;
-		player.setTempo(tempo);
-		player.loadSamples(seq);
-	  })
-	  .catch(error => console.error(`Failed to load motif ${motif}:`, error));
-  }
-
-
+    const audioElement = document.createElement('audio');
+    audioElement.style.display = "none";
+    audioElement.src = `/writings/2025/a-review-of-the-boy-and-the-heron/assets/motifs/${motif}.mp3`;
+    audioElement.controls = true;
+    audioElement.volume = 0.5;
+    audioElements[i] = audioElement;
+    
+    staff.parentNode.appendChild(audioElement);
+    staff.addEventListener('click', () => startOrStop(i));
+    
+    mm.urlToNoteSequence(`/writings/2025/a-review-of-the-boy-and-the-heron/assets/motifs/${motif}.mid`)
+        .then(seq => {
+            sequences[i] = seq;
+            visualizers[i] = new mm.StaffSVGVisualizer(seq, staff);
+            player.loadSamples(seq); // preload samples
+        })
+        .catch(error => console.error(`Failed to load motif ${motif}:`, error));
+}
 
 function startOrStop(index) {
-	if (player.isPlaying()) {
-		player.stop();
-		music_players[index].pause();
-		music_players[index].current_time = 0;
-
-		for (let i = 0; i < staffs.length; i++) {
-			staffs[i].setAttribute("playing", false);
-			if (visualizers[i]) {
-				visualizers[i].clearActiveNotes();
-			}
-		}
-	} else {
-		if (music_players[index] != null) {
-			mm.Player.tone.Master.volume.value - Infinity;
-			music_players[index].play();
-		} else {
-			mm.Player.tone.Master.volume.value = 0;
-		}
-		player.start(sequences[index]);
-		staffs[index].setAttribute("playing", true);
+	if (sequences[index].tempos && sequences[index].tempos.length > 0) {
+		player.setTempo(sequences[index].tempos[0].qpm);
 	}
-	last_index = index;
+	// already playing something
+    if (currentlyPlayingIndex !== -1) {
+        player.stop();
+		// lol this doesn't work in stop i gotta do this out here
+		for (let i = 0; i < visualizers.length; i++) {
+			visualizers[i].clearActiveNotes();
+			visualizers[i].clear();
+			visualizers[i].redraw(null, false);
+		}
+        
+        if (audioElements[currentlyPlayingIndex]) {
+            audioElements[currentlyPlayingIndex].pause();
+            audioElements[currentlyPlayingIndex].currentTime = 0;
+        }
+        
+        // visual state
+        staffs[currentlyPlayingIndex].setAttribute("playing", "false");
+        if (visualizers[currentlyPlayingIndex]) {
+            visualizers[currentlyPlayingIndex].clearActiveNotes();
+        }
+        
+		// if we click on what we're playing just stop
+        if (currentlyPlayingIndex === index) {
+            currentlyPlayingIndex = -1;
+            return;
+        }
+    }
+    
+    currentlyPlayingIndex = index;    
+	staffs[index].setAttribute("playing", "true");
+    
+    // use audio, fallback to midi
+    if (audioElements[index] != null) {
+        player.output.volume.value = -Infinity;
+        player.start(sequences[index]);
+        audioElements[index].currentTime = 0;
+        audioElements[index].play().catch(err => {
+            console.error("Audio playback failed:", err);
+            player.output.volume.value = 0;
+        });
+    } else {
+        player.output.volume.value = 0;
+        player.start(sequences[index]);
+    }
 }
